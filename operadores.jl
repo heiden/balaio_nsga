e882f05b586f96c87a262949bc3aed897f909787
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-03-11 14:45:27 -0300

    crossover ok

--- operadores.jl
+++ operadores.jl
@@ -99,6 +99,81 @@ function meu_crossover(solver::NSGA, selecao)
 
 end
 
+function crossover(solver::NSGA, selecao, k)
+	# uniforme pros lotes?
+	# pros ativos vou implementar a do DEB
+
+	tam = length(solver.populacao[1].ativos)
+	for s in selecao
+		# ativos
+    	pa_lotes,  pb_lotes  = solver.populacao[s[1]].lotes,  solver.populacao[s[2]].lotes
+    	pa_ativos, pb_ativos = copy(solver.populacao[s[1]].ativos), copy(solver.populacao[s[2]].ativos)
+    	ω = rand()
+    	if ω < solver.cx
+	    	fa_ativos, fb_ativos = [0 for x in 1:tam], [0 for x in 1:tam]
+	    	# se os dois forem 0, pega 0 // se os dois forem 1, pega 1
+	    	cunt = 0
+	    	for i in 1:tam
+	    		if pa_ativos[i] == 1 && pb_ativos[i] == 1
+	    			cunt += 1
+	    			fa_ativos[i], fb_ativos[i] = 1, 1
+	    			pa_ativos[i], pb_ativos[i] = -1, -1 # anula o indice i pra nao pegar dnv no sorteio depois
+	    		elseif pa_ativos[i] == 0 && pb_ativos[i] == 0
+	    			fa_ativos[i], fb_ativos[i] = 0, 0
+	    			pa_ativos[i], pb_ativos[i] = -1, -1 # anula o indice i pra nao pegar dnv no sorteio depois
+	    		end
+	    	end
+	    	# pega todos os indices que são 1 nos pais
+	    	qnt = k - cunt
+	    	uns = findall(isequal(1), pa_ativos)
+	    	outros = findall(isequal(1), pb_ativos)
+	    	uns_e_outros = [uns; outros]
+	    	escolhas_fa = sample(uns_e_outros, qnt, replace = false)
+	    	escolhas_fb = sample(uns_e_outros, qnt, replace = false)
+	    	for i in 1:qnt
+	    		fa_ativos[escolhas_fa[i]] = 1
+	    		fb_ativos[escolhas_fb[i]] = 1
+	    	end
+	    
+	    	# println("pa: ", solver.populacao[s[1]].ativos)
+	    	# println("pb: ", solver.populacao[s[2]].ativos)
+	    	# println("fa: Int32", fa_ativos)
+	    	# println("fb: Int32", fb_ativos)
+	    	# exit(0)
+
+    		pa_lotes_atual, pb_lotes_atual = filter(x->x≠0, pa_lotes), filter(x->x≠0, pb_lotes)
+	    	fa_lotes, fb_lotes = [0.0 for x in 1:tam], [0.0 for x in 1:tam]
+
+	    	tam_lotes = length(pa_lotes_atual)
+	    	for i in 1:tam_lotes
+	    		valor = (pa_lotes_atual[i] + pb_lotes_atual[i]) / 2
+	    		pa_lotes_atual[i], pb_lotes_atual[i] = valor, valor
+	    	end
+
+	    	cunt_a = 0
+	    	cunt_b = 0
+	    	for i in 1:tam
+	    		if fa_ativos[i] == 1
+	    			fa_lotes[i] = pa_lotes_atual[cunt_a+=1]
+	    		else
+	    			fa_lotes[i] = 0
+	    		end
+	    		if fb_ativos[i] == 1
+	    			fb_lotes[i] = pb_lotes_atual[cunt_b+=1]
+	    		else
+	    			fb_lotes[i] = 0
+	    		end
+	    	end
+	    	# ind.lotes /= sum(ind.lotes) # não precisa reparar do jeito que implementei
+
+			push!(solver.populacao, Individuo(fa_ativos, fa_lotes))
+			push!(solver.populacao, Individuo(fb_ativos, fb_lotes))
+
+	    end
+	end
+
+end
+
 function mutacao(solver::NSGA)
     # bit flip pros ativos, vai gerar indivíduos inválidos
     # reparar pegando um oposto e flipando ele tbm
@@ -154,11 +229,10 @@ function mutacao(solver::NSGA)
     			lotes_atuais[i] += pert
     		end
     	end
-    	cont = 1
+    	cunt = 0
     	for i in 1:tam
     		if ind.ativos[i] == 1
-    			ind.lotes[i] = lotes_atuais[cont]
-    			cont += 1
+    			ind.lotes[i] = lotes_atuais[cunt+=1]
     		else
     			ind.lotes[i] = 0
     		end
